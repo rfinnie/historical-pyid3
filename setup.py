a1cc6992c622f2a1e3f4fa68eaaf18c75dc3963a
Ryan Finnie <ryan@finnie.org>
2001-08-11 00:23:01 -0700

    Added package files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name="PyID3",
+  url="http://pyid3.sourceforge.net/",
+  author="Ryan Finnie",
+  author_email="ryan@finnie.org",
+  version="0.0.1",
+  packages=["id3"]
+)
+
